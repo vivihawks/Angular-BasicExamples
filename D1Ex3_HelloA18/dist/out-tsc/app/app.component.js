2c4222a8c6b6b382aeb87a4601e195ac6139d9b5
Vignesh Murali Natarajan <vivihawks@yahoo.co.in>
2024-06-07 10:10:37 +0530

    Angular 18 Upgrade

---
+++ D1Ex3_HelloA18/dist/out-tsc/app/app.component.js
@@ -0,0 +1,20 @@
+import { __decorate } from "tslib";
+import { Component } from '@angular/core';
+let AppComponent = class AppComponent {
+    constructor() {
+        this.title = 'NFEx_Sample';
+        this.y = 0;
+    }
+    test() {
+        var x = 0;
+    }
+};
+AppComponent = __decorate([
+    Component({
+        selector: 'app-root',
+        templateUrl: './app.component.html',
+        styleUrls: ['./app.component.scss']
+    })
+], AppComponent);
+export { AppComponent };
+//# sourceMappingURL=app.component.js.map
